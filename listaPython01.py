ea01b54e836c7bbcc4953f67a3e32f5d1110f9f3
paola-rodrigues <paola.n.rodrigues@gmail.com>
2021-09-22 12:21:32 -0300

    primeiro commit

---
+++ listaPython01.py
@@ -0,0 +1,34 @@
+Python 3.9.7 (tags/v3.9.7:1016ef3, Aug 30 2021, 20:19:38) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a = 10
+>>> print (a)
+10
+>>> type(a)
+<class 'int'>
+>>> b = 2/3
+>>> print (b)
+0.6666666666666666
+>>> type (b)
+<class 'float'>
+>>> a = 2
+>>> b = 3
+>>> c = 6
+>>> print ( a + b + c )
+11
+>>> d =3**2
+>>> print (d)
+9
+>>> x = 20/5
+>>> print (x)
+4.0
+>>> #6 questão F = C∗(9.0/5.0)+32.0
+>>> C = 1
+>>> d = (9.0/5.0) + 32.0
+>>> print (C * d)
+33.8
+>>> # 7 questão C = 5.0 ∗ (F − 32.0)/9.0
+>>> f = 41
+>>> e = 5.0 * ((f -32.0)/9.0)
+>>> print (e)
+5.0
+>>> 
