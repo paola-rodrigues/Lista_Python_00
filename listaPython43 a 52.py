764ef2b957db9a917049d42f8344c3bd39283bb4
paola-rodrigues <paola.n.rodrigues@gmail.com>
2021-09-26 19:19:34 -0300

    Update listaPython43 a 52.py

--- listaPython43 a 52.py
+++ listaPython43 a 52.py
@@ -7,3 +7,41 @@ print("Questão 42")
 salario_base = float(input("Digite o valor do salario-base: "))
 salario_receber = (salario_base * 1.05) - (salario_base * 0.07)                    
 print(" O salário a receber é R$ {}".format(salario_receber))
+
+'''
+43. Escreva um programa de ajuda para vendedores. A partir de um valor total lido, mostre:
+• o total a pagar com desconto de 10%;
+• o valor de cada parcela, no parcelamento de 3× sem juros;
+• a comissao do vendedor, no caso da venda ser a vista (5% sobre o valor com des- ˜
+conto)
+• a comissao do vendedor, no caso da venda ser parcelada (5% sobre o valor total)
+
+'''
+
+
+
+
+'''
+44.Receba a altura do degrau de uma escada e a altura que o usuario deseja alcanc¸ar ´
+subindo a escada. Calcule e mostre quantos degraus o usuario dever ´ a subir para atingir ´
+seu objetivo
+'''
+
+
+
+'''
+45.Fac¸a um programa que leia um numero inteiro positivo de tr ´ es d ˆ ´ıgitos (de 100 a 999).
+Gere outro numero formado pelos d ´ ´ıgitos invertidos do numero lido. Exemplo:
+'''
+
+
+
+'''
+46. Leia um numero inteiro de 4 d ´ ´ıgitos (de 1000 a 9999) e imprima 1 d´ıgito por linha
+'''
+
+
+'''
+47.Leia um valor inteiro em segundos, e imprima-o em horas, minutos e segundos
+'''
+
